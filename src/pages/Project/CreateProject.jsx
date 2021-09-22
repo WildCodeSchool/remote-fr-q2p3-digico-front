7c679f22ba150a6305f64b3a27a5e7bd140010af
mathieumussano <mussanomathieu@gmail.com>
2021-09-22 11:05:28 +0200

    connectfooterOK

--- src/pages/Project/CreateProject.jsx
+++ src/pages/Project/CreateProject.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import ConnectFooter from '../../components/footer/ConnectFooter'
+
+
 import "./CreateProject.css"
 
 function CreateProject() {
     return (
         <div>
             Ceci est la création de projet
+            <ConnectFooter/> 
         </div>
+
+            
     )
 }
 
