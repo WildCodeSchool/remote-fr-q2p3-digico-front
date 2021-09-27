75e9bf4eca30545e98dfa4ede349259b27faad9d
mathieumussano <mussanomathieu@gmail.com>
2021-09-27 16:28:35 +0200

    mise en css clean code

--- src/pages/Contribution.jsx
+++ src/pages/Contribution.jsx
@@ -11,9 +11,12 @@ function Contribution() {
             <ConnectFooter/>
             <div className="form_contrib">
             <label className="dispo_contrib"> Mes disponibilités</label>
+            <label className="dispo_contrib_start"> A partir du :</label>
+            <input className="date_contrib" type="date" />
+            <label className="dispo_contrib_end">Jusqu'au :</label>
             <input className="date_contrib" type="date" />
             <label className="title_desc_contrib">Dis nous en plus</label>
-            <input className="description_contribution" type="textarea" placeholder="juste quelques lignes pour expliquer ce que tu pourrais apporter"/>
+            <textarea className="description_contribution" type="textarea"  placeholder="juste quelques lignes pour te présenter nous expliquer ce que tu pourrais apporter au projet "/>
             <button className="button_submit_contrib">JE ME PROPOSE</button>
 
             </div>   
