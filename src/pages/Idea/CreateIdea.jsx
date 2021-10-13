32ab08094224cb0e1b1eb9afb3a4b4edbfc3efab
sarah <sarah.duchaussoy@hotmail.fr>
2021-10-13 15:43:16 +0200

    create idea form done

--- src/pages/Idea/CreateIdea.jsx
+++ src/pages/Idea/CreateIdea.jsx
@@ -1,28 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+import axios from 'axios';
 import ConnectFooter from '../../components/footer/ConnectFooter'
 import Navbar from '../../components/Navbar';
 import "./CreateIdea.css";
 
 
 function CreateIdea() {
+  let history = useHistory();
+  const url = "http://localhost:8000/api/ideas"
+  const [category, setCategory] = useState("")
+  const [idea, setIdea] = useState({
+    title: "",
+    // category: "",
+    description: "",
+    idea_date: "",
+    user_id: "",
+  }) 
+
+  const createIdea = e => {
+    e.preventDefault();
+    axios.post(url,{
+      title: idea.title,
+      category: category,
+      description: idea.description,
+      idea_date: new Date().toLocaleDateString('fr-FR'),
+      user_id: idea.user_id
+    })
+    alert("Votre idée a été crée.")
+    history.push('/liste-idee')
+  }
+  // function handleCategory(e) {
+  //   const newCategory
+  // }
+  function handleInputChange(e) {
+    const newIdea= {...idea}
+    newIdea[e.target.id] = e.target.value
+    setIdea(newIdea)
+  }
+
     return (
         <div>
             <ConnectFooter/> 
-            <form className="create-idea" >
+            <form className="create-idea" onSubmit={createIdea}>
                 <label>Titre de votre idée : </label>
-                <input className="idea-form-title" name="title" type="textarea"/>
+                <input required className="idea-form-title" name="title" id="title" type="textarea" onChange={(e) => handleInputChange(e)} value={idea.title}/>
                 <label className="categorie-label">Catégorie : </label>
-                <select className="categorie-list-idea" name="categorieList" id="selectList">
+                <select required className="categorie-list-idea" name="categorieList" id="category" onChange={(e) => setCategory(e.target.value)} value={category}>
                 <option value=""> -- Choisir une catégorie -- </option>
-                    <option value="option 1">Ecologie</option>
-                    <option value="option 2">Social</option>
-                    <option value="option 3">Tech</option>
+                    <option value="Ecologie">Ecologie</option>
+                    <option value="Social">Social</option>
+                    <option value="Tech">Tech</option>
+                    <option value="Tech">Divers</option>
                 </select>
-                <label className="img-label"> Image (facultatif) : </label>
-                <input className="idea-form-img" name="image" type="file"/>
                 <label>Description détaillée : </label>
-                <textarea className="idea-form-desc" name="desc" type="textarea" placeholder="Ceci est la description"/>
-                <input className="create-idea_submit-btn" type="submit"/>
+                <textarea required className="idea-form-desc" name="desc" id="description" type="textarea" onChange={(e) => handleInputChange(e)} value={idea.description}/>
+                <label for="user"> <br/>User ID :</label>
+                <input required type="number" id="user_id"
+                  onChange ={(e) => handleInputChange(e)}
+                  value={idea.user_id}
+                  className="contributors_input"/> 
+                <input className="create-idea_submit-btn" type="submit" value="Publier"/>
             </form>
             <Navbar/>
           </div>
